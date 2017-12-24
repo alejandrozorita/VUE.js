c41b4adadf9b2d74af98853008a7343dba194a2a
Alejandro Zorita <alejandro.zorita@medssocial.com>
2017-12-24 17:23:11 +0100

    Renderizado listas

---
+++ 2-renderizado-de-listas/js/main.js
@@ -0,0 +1,20 @@
+const vm = new Vue({
+
+    el: 'main',
+
+    data: {
+        dias: ['Lunes', 'Martes', 'Míercoles', 'Jueves', 'Viernes'],
+
+        tareas: [
+            { nombre: 'Estudiar', prioridad: 'baja' },
+            { nombre: 'Vuejs', prioridad: 'alta' },
+            { nombre: 'Firebase', prioridad: 'media' },
+        ],
+
+        persona: {
+            nombre: 'Alejandro',
+            profesion: 'Developer',
+            ciudad: 'Madrid'
+        }
+    },
+})
