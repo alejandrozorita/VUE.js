2b4fd28cebcb95a99fa0d59bff985df3a22303f9
Alejandro Zorita <alejandro.zorita@medssocial.com>
2018-01-01 21:20:52 +0100

    Transiciones VUEjs

---
+++ 8-Transiciones/js/main.js
@@ -0,0 +1,16 @@
+const vm = new Vue({
+
+    el: 'main',
+
+    data: {
+
+        mostrar: true,
+
+        mensajes: {
+            transicion: 'Transición CSS con VueJS',
+            animacion: 'Animación CSS con VueJS',
+            animacioncustom: 'Animaciones custom CSS con VueJS',
+            entreElementos: 'Transiciones entre elementos con VueJS',
+        },
+    }
+});
